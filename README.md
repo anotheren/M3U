c6d27bf41acb04e56764a8b5f5d41335c8df2b5a
anotheren <liudong.edward@gmail.com>
2022-03-29 22:46:12 +0800

    chore: cache files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # M3U
 M3U Parser, write in Swift
+
+[RFC8162](https://datatracker.ietf.org/doc/html/rfc8216)
--- Tests/M3UTests/M3UTests.swift
+++ Tests/M3UTests/M3UTests.swift
@@ -6,37 +6,51 @@ import Alamofire
 /// https://developer.apple.com/streaming/examples/
 private enum AppleHLSTestURL: String, CaseIterable {
     
-    /// Advanced stream
+    /// Advanced stream - TS
+    /// TS stream compatible with macOS v10.7 or later, iOS 6 or later, and tvOS 9 or later
+    case advancedStream_TS = "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8"
+    
+    /// Advanced stream - fMP4
     /// fMP4 stream compatible with macOS v10.12 or later, iOS 10 or later, and tvOS 10 or later
-    case advancedStream = "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8"
+    case advancedStream_fMP4 = "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8"
     
     /// Advanced stream (HEVC/H.264)
     /// Stream backwards compatible with macOS v10.7 or later, iOS 6 or later, and tvOS 9 or later
     /// HEVC variants compatible with macOS v10.13 or later, iOS 11 or later, and tvOS 11 or later
-    case advancedStreamHEVC = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8"
+    case advancedStream_HEVC = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8"
     
     var url: URL { URL(string: rawValue)! }
 }
 
 final class M3UTests: XCTestCase {
     
-    func testAppleHLSTestURLs() async throws {
-        for appleHlS in AppleHLSTestURL.allCases {
-            try await test(url: appleHlS.url)
+    func testAppleHLS_advancedStream_TS() async throws {
+        let masterURL = AppleHLSTestURL.advancedStream_TS.url
+        let masterPlayList = try await loadM3U(url: masterURL)
+        
+//        print(masterPlayList)
+        
+        
+        if let stream_inf = masterPlayList.tags.compactMap({ $0 as? EXT_X_STREAM_INF }).randomElement() {
+            let url = masterURL.deletingLastPathComponent().appendingPathComponent(stream_inf.uri)
+            print(url)
+            let playList = try await loadM3U(url: url)
+            print(playList)
+            print(playList.plainText)
         }
     }
 }
 
 extension M3UTests {
     
-    private func test(url: URL) async throws {
+    private func loadM3U(url: URL) async throws -> M3U {
         let request = AF.request(url)
         let dataTask = request.serializingData()
         let data = try await dataTask.value
         let plainText = String(data: data, encoding: .utf8)!
         let m3u = M3U(plainText: plainText)!
-        print(m3u)
         let newPlainText = m3u.plainText
         XCTAssertTrue(plainText == newPlainText)
+        return m3u
     }
 }
